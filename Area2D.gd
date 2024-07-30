b1afe40ddcd65e6e590e5b87405ee1a6281a12f1
TimmyQuimpo <timmy.quimpo@student.onslow.school.nz>
2024-07-30 18:10:48 +1200

    Baseline (and I really mean it) collision

--- Area2D.gd
+++ Area2D.gd
@@ -2,10 +2,9 @@ extends Area2D
 
 ## Credit to Coding Quests
 
-var previous_position = [0, 0]
+## Find way to set position based off of tilemap coordinates
 @export var current_position = [0, 0]
 @export var step_size: int = 25
-
 
 func _on_floor_0_give_start_coords(starting_coords_global):
 	print(starting_coords_global)
@@ -15,9 +14,9 @@ func _ready():
 	pass
 
 func _input(event):
-	previous_position = current_position
 	if event.is_action_pressed("step_up"):
 		current_position[1] -= step_size
+		## Testing
 	if event.is_action_pressed("step_down"):
 		current_position[1] += step_size
 	if event.is_action_pressed("step_right"):
@@ -25,8 +24,10 @@ func _input(event):
 	if event.is_action_pressed("step_left"):
 		current_position[0] -= step_size
 	
+	print(overlaps_body(Wall_1))
+	
 	self.position = Vector2(current_position[0], current_position[1])
 
 
-func _on_body_entered(body):
-	current_position = previous_position
+
+
