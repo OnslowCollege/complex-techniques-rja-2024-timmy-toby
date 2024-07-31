28dc6ad1a45a1e3a3858c1a090ad166198822655
TimmyQuimpo <timmy.quimpo@student.onslow.school.nz>
2024-07-31 12:33:26 +1200

    Adding testing print functions & resolving starting position bug

--- Player.gd
+++ Player.gd
@@ -9,7 +9,10 @@ var previous_position = [0, 0]
 
 func _on_floor_0_give_start_coords(starting_coords_global):
 	print(starting_coords_global)
+	previous_position = starting_coords_global
 	current_position = starting_coords_global
+	self.position = Vector2(current_position[0], current_position[1])
+	
 
 func _ready():
 	pass
@@ -17,16 +20,21 @@ func _ready():
 func _input(event):
 	previous_position = current_position
 	if event.is_action_pressed("step_up"):
+		print("Up")
 		current_position[1] -= step_size
 	if event.is_action_pressed("step_down"):
+		print("Down")
 		current_position[1] += step_size
 	if event.is_action_pressed("step_right"):
+		print("Left")
 		current_position[0] += step_size
 	if event.is_action_pressed("step_left"):
+		print("Right")
 		current_position[0] -= step_size
 	
 	self.position = Vector2(current_position[0], current_position[1])
 
 
 func _on_body_entered(body):
+	print("Blocked")
 	current_position = previous_position
