dfe48686eab9e62b025c90fdb618a54d494fdd23
yobsicle <124639826+yobsicle@users.noreply.github.com>
2024-08-19 09:21:41 +1200

    finished character select
    Added character information in tooltips, and basic functionality for the buttons

--- Character select.gd
+++ Character select.gd
@@ -1,5 +1,20 @@
 extends Control
 
+
+## Related to players saved stats
+var player_class: String
+var health: int
+var stamina: int
+var karma: int
+var karma_level: int # to help id the level the player should be returned to.
+
+
+## Path to relevant save file
+const position_save_path = "res://Game/position.save"
+const player_save_path = "res://Game/player.save" 
+const combat_save_path = "res://Game/combat.save"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Control_acolyte/Acolyte_label.hide()
@@ -11,69 +26,112 @@ func _ready():
 
 
 func _on_acolyte_pressed():
-	pass # Replace with function body.
+	player_class = "Acolyte"
+	health = 100
+	stamina = 100
+	karma = 0
+	karma_level = 0
+	# Put scene change here Timmy
 
 
 func _on_mage_pressed():
-	pass # Replace with function body.
+	player_class = "Mage"
+	health = 100
+	stamina = 100
+	karma = 0
+	karma_level = 0
+	# Put scene change here Timmy
 
 
 func _on_paladin_pressed():
-	pass # Replace with function body.
+	player_class = "Paladin"
+	health = 100
+	stamina = 100
+	karma = 0
+	karma_level = 0
+	# Put scene change here Timmy
 
 
 func _on_performer_pressed():
-	pass # Replace with function body.
+	player_class = "Performer"
+	health = 100
+	stamina = 100
+	karma = 0
+	karma_level = 0
+	# Put scene change here Timmy
 
 
 func _on_warrior_pressed():
-	pass # Replace with function body.
+	player_class = "Warrior"
+	health = 100
+	stamina = 125
+	karma = 0
+	karma_level = 0
+	# Put scene change here Timmy
 
 
-func _on_control_acolyte_mouse_entered():
-	print("emitted")
-	$Control_acolyte/Acolyte_label.show()
-
-func _on_control_acolyte_mouse_exited():
-	print("emitted")
-	$Control_acolyte/Acolyte_label.hide()
-
-
-func _on_control_mage_mouse_entered():
-	print("emitted")
+func _on_mage_mouse_entered():
 	$Control_Mage/Mage_label.show()
 
 
-func _on_control_mage_mouse_exited():
-	print("emitted")
+func _on_mage_mouse_exited():
 	$Control_Mage/Mage_label.hide()
 
 
-func _on_control_paladin_mouse_entered():
-	print("emitted")
-	$Control_Paladin/Paladin_label.show()
-
-
-func _on_control_paladin_mouse_exited():
-	print("emitted")
-	$Control_Paladin/Paladin_label.hide()
-
-
-func _on_control_performer_mouse_entered():
-	print("emitted")
+func _on_performer_mouse_entered():
 	$Control_performer/Performer_label.show()
 
 
-func _on_control_performer_mouse_exited():
-	print("emitted")
+func _on_performer_mouse_exited():
 	$Control_performer/Performer_label.hide()
 
 
-func _on_control_warrior_mouse_entered():
-	print("emitted")
+func _on_warrior_mouse_entered():
 	$Control_warrior/Warrior_label.show()
 
 
-func _on_control_warrior_mouse_exited():
-	print("emitted")
+func _on_warrior_mouse_exited():
 	$Control_warrior/Warrior_label.hide()
+
+
+func _on_acolyte_mouse_entered():
+	$Control_acolyte/Acolyte_label.show()
+
+
+func _on_acolyte_mouse_exited():
+	$Control_acolyte/Acolyte_label.hide()
+
+
+func _on_paladin_mouse_entered():
+	$Control_Paladin/Paladin_label.show()
+
+
+func _on_paladin_mouse_exited():
+	$Control_Paladin/Paladin_label.hide()
+
+
+func saveplayer():
+	var file = FileAccess.open(player_save_path, FileAccess.WRITE)
+	file.store_var(player_class)
+	file.store_var(health)
+	file.store_var(stamina)
+	file.store_var(karma)
+	file.store_var(karma_level)
+
+
+func loadplayer():
+	if FileAccess.file_exists(player_save_path):
+		var file = FileAccess.open(player_save_path, FileAccess.READ)
+		
+		player_class = file.get_var()
+		print(player_class, " is the player class")
+		health = file.get_var()
+		print(health, " is the player's health")
+		stamina = file.get_var()
+		print(stamina, " is the players stamina")
+		karma = file.get_var()
+		print(karma, " is the players karma")
+		karma_level = file.get_var()
+		print(karma_level, " is the players karma level")
+	else:
+		print("No such file")
